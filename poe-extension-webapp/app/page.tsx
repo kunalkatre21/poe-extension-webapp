2f0f3b1ba6222d294b5ecb56da0147f9c37c193f
kunalkatre21 <kunalkatre.designs@gmail.com>
2024-09-08 15:08:18 +0530

    Initial commit - coming soon page

---
+++ poe-extension-webapp/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-white text-6xl font-bold">Coming Soon...</h1>
+    </div>
+  );
+}
